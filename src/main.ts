0590a36b662b04b5604ce63cce1a0e723291ca07
mfelipperd <felipperabelodurans@gmail.com>
2025-04-23 18:21:45 -0300

    feat: use validationPipe

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(process.env.PORT ?? 8000);
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
 }
 bootstrap();
