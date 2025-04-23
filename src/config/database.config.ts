4070649868c8757b8a0d96619f5ac36c0e1c696e
mfelipperd <felipperabelodurans@gmail.com>
2025-04-23 19:45:50 -0300

    feat: change db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,18 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { Visitor } from 'src/modules/visitors/entities/visitor.entity';
 import { Fair } from 'src/modules/fairs/entity/fair.entity';
 
-ConfigModule.forRoot();
-
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '3306', 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: true, // cuidado: apenas para desenvolvimento
   entities: [Visitor, Fair],
 };
