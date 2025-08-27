a54f726d4e5e396f3324b90d4735be64338c5d5c
mfelipperd <felipperabelodurans@gmail.com>
2025-08-27 13:31:28 -0300

    save db changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,11 +4,15 @@ dotenv.config(); // força leitura caso falhe no Nest
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Visitor } from 'src/modules/visitors/entities/visitor.entity';
 import { Fair } from 'src/modules/fairs/entity/fair.entity';
+import { Expense } from 'src/modules/finance/expenses/entities/expense.entity';
+import { FinanceCategory } from 'src/modules/finance/common/entities/finance-category.entity';
+import { Account } from 'src/modules/finance/common/entities/account.entity';
+import { CashFlow } from 'src/modules/finance/cash-flow/entities/cash-flow.entity';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   url: process.env.DATABASE_URL?.trim(),
   autoLoadEntities: true,
   synchronize: true,
-  entities: [Visitor, Fair],
+  entities: [Visitor, Fair, Expense, FinanceCategory, Account, CashFlow],
 };
