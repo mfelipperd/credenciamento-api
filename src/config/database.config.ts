e5cefe8cb61f3423e83bb0641cb967c83b764675
mfelipperd <felipperabelodurans@gmail.com>
2026-08-21 17:08:00 -0300

    fix(database): disable schema sync in production

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -25,7 +25,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   url: process.env.DATABASE_URL?.trim(),
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: process.env.RAILWAY_ENVIRONMENT_NAME !== 'production',
   entities: [
     Visitor,
     Fair,
